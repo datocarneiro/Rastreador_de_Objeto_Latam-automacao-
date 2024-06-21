bd1c93dc82a355629d42d521a051a485d9aef126
datocarneiro <132966071+datocarneiro@users.noreply.github.com>
2024-06-21 13:29:40 -0300

    update exportação dos resultados  em excel

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import time
+import io
+import pickle
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, request, session, send_file
 from openpyxl import load_workbook
 from werkzeug.utils import secure_filename
 from selenium.webdriver.support.ui import WebDriverWait
@@ -15,26 +17,13 @@ from selenium.common.exceptions import TimeoutException
 app = Flask(__name__)
 app.secret_key = 'dato123'  # Defina uma chave secreta adequada
 
-##########################################################################
-'''
-# para rodar no replit usar essas configuraçõa
-options = Options()
-options.add_argument("--no-sandbox")
-options.add_argument("--disable-dev-shm-usage")
-options.headless = False  # Executar o Chrome de forma oculta
-
-driver = webdriver.Chrome(options=options)
-'''
-##########################################################################
+# Configuração do Selenium
 from webdriver_manager.chrome import ChromeDriverManager
 servico = Service(ChromeDriverManager().install())
-# para rodar no replit usar essas configuraçõa
 options = Options()
 options.add_argument("--no-sandbox")
-options.add_argument("--headless")      # Executar o Chrome de forma oculta
+options.add_argument("--headless")  # Executar o Chrome de forma oculta
 options.add_argument("--disable-dev-shm-usage")
-options.headless = True  
-
 driver = webdriver.Chrome(options=options)
 
 # Variável global para armazenar a lista de pendentes
@@ -48,7 +37,26 @@ def index():
 def resultado():
     statuses, datas, df = capturar_status_pendentes()
     table_html = df.to_html(classes='table table-bordered', index=False)
+    # Armazenar o DataFrame na sessão como um pickle
+    session['df'] = pickle.dumps(df)
     return render_template('resultado.html', table_html=table_html)
+
+@app.route('/exportar_excel')
+def exportar_excel():
+    # Reutilizar o DataFrame da sessão
+    df_pickle = session.get('df')
+    if not df_pickle:
+        return "Nenhum dado disponível para exportação"
+
+    df = pickle.loads(df_pickle)
+    
+    # Utilizando um buffer em memória para salvar o DataFrame como Excel
+    output = io.BytesIO()
+    with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
+        df.to_excel(writer, index=False, sheet_name='Sheet1')
+    output.seek(0)
+    
+    return send_file(output, mimetype='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', download_name='resultado.xlsx', as_attachment=True)
 
 @app.route('/', methods=['POST'])
 def preparar_dados_planilha():
@@ -67,45 +75,37 @@ def preparar_dados_planilha():
     aba_ativa = planilha.active
 
     lista_pendentes = []
-    for coluna_a, coluna_c, coluna_d in zip(aba_ativa["A"][1:],
-                                            aba_ativa["C"][1:],
-                                            aba_ativa["D"][1:]):
+    for coluna_a, coluna_c, coluna_d in zip(aba_ativa["A"][1:], aba_ativa["C"][1:], aba_ativa["D"][1:]):
         if coluna_d.value != 'ENTREGUE':
             if coluna_c.value is not None:
                 lista_pendentes.append(coluna_c.value)
-
+    total_pendentes = len(lista_pendentes)
     print("=" * 150)
-    print(f'As pendente de entrega são: {lista_pendentes}')
+    print(f'As pendente de entrega são: Total: |{total_pendentes}| {lista_pendentes}')
     print("=" * 150)
 
-    return render_template('index.html', pendentes=lista_pendentes)
+    return render_template('index.html', total_pendentes=total_pendentes, pendentes=lista_pendentes)
 
-def captura_status(awb):
-    driver.get(
-        f"https://www.latamcargo.com/en/trackshipment?docNumber={awb}&docPrefix=957&soType=SO"
-    )
-
+def captura_status(franquia, awb):
+    driver.get(f"https://www.latamcargo.com/en/trackshipment?docNumber={awb}&docPrefix=957&soType=SO")
     wait = WebDriverWait(driver, 30)
-
     try:
-        status_evento = wait.until(
-            EC.visibility_of_element_located(
-                (By.XPATH, '//*[@id="statusTable"]/tbody/tr[1]/td[1]')))
+        status_evento = wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="statusTable"]/tbody/tr[1]/td[1]')))
         status = status_evento.text
 
-        data_evento = wait.until(
-            EC.visibility_of_element_located(
-                (By.XPATH, '//*[@id="statusTable"]/tbody/tr[1]/td[6]')))
+        data_evento = wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="statusTable"]/tbody/tr[1]/td[6]')))
         data = data_evento.text
-
-        captura = f'dados capturados,AWB,{awb},STATUS,{status},DATA_EVENTO,{data}'
+        total = len(lista_pendentes)
+        captura = f'|TOTAL: {total}|FRANQUIA: {franquia}|AWB: {awb}| STATUS: {status}|DATA_EVENTO: {data}|'
+        print("=" * 150)
         print(captura)
+        print("=" * 150)
 
         return status, data
     except TimeoutException:
-        status = "Erro de tempo limite"
-        data = "Erro de tempo limite"
-        print("Erro de tempo limite ao capturar os dados")
+        status = "Erro de tempo limite (latam indisponível)"
+        data = "Erro de tempo limite (latam indisponível)"
+        print("Erro de tempo limite (latam indisponível)")
         return status, data
 
 def capturar_status_pendentes():
@@ -120,14 +120,14 @@ def capturar_status_pendentes():
         planilha = load_workbook(excel_filename)
         aba_ativa = planilha.active
 
-        for coluna_a, coluna_c, coluna_d in zip(aba_ativa["A"][1:],
-                                                aba_ativa["C"][1:],
-                                                aba_ativa["D"][1:]):
+        for coluna_a, coluna_c, coluna_d in zip(aba_ativa["A"][1:], aba_ativa["C"][1:], aba_ativa["D"][1:]):
             if coluna_d.value != 'ENTREGUE':
                 if coluna_c.value is not None:
+                    franquia = coluna_a.value
                     awb = coluna_c.value
-                    status, data = captura_status(awb)
+                    status, data = captura_status(franquia, awb)
                     dados_rastreamento.append({
+                        'FRANQUIA': franquia,
                         'AWB': awb,
                         'STATUS': status,
                         'DATA_EVENTO': data
@@ -136,7 +136,6 @@ def capturar_status_pendentes():
                     datas.append(data)
 
         df = pd.DataFrame(dados_rastreamento)
-
         return statuses, datas, df
     else:
         return [], [], None  # Retorna vazios se o arquivo não estiver definido na sessão
